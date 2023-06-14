d431be1570f983a93b0c9116dc1215be52419e7b
hugomeinhof <hugo@raballo.de>
2023-06-14 14:59:34 +0200

    now with catppuccin and adjusted comment colour

--- chadrc.lua
+++ chadrc.lua
@@ -2,6 +2,24 @@
 local M = {}
 M.mappings = require 'custom.mappings'
 M.plugins = 'custom.plugins'
-M.ui = {theme = 'chadracula'}
+--
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = 'catppuccin',
+  hl_override = highlights.override,
+  hl_add = highlights.add, nvdash = {
+    load_on_startup = true,
+    header = {
+    "┬ ┬┌─┐┬ ┬  ┌─┐┌─┐┌┐┌┌┬┐  ┌─┐┌─┐┌┬┐┌─┐┬ ┬  ┌─┐┌─┐┌─┐┌─┐┌─┐┌─┐  ",
+    "└┬┘│ ││ │  │  ├─┤│││ │   ├┤ ├┤  │ │  ├─┤  │  │ │├┤ ├┤ ├┤ ├┤   ",
+    " ┴ └─┘└─┘  └─┘┴ ┴┘└┘ ┴   └  └─┘ ┴ └─┘┴ ┴  └─┘└─┘└  └  └─┘└─┘  ",
+    "┬┌─┐  ┬ ┬┌─┐┬ ┬┬─┐┌─┐  ┌┬┐┌─┐┌─┐┌┬┐                           ",
+    "│├┤   └┬┘│ ││ │├┬┘├┤    ││├┤ ├─┤ ││                           ",
+    "┴└     ┴ └─┘└─┘┴└─└─┘  ─┴┘└─┘┴ ┴─┴┘o                          ",
+    }
+  }
+}
 
 return M
---
+++ highlights.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+M.override = {
+  Comment = {
+    italic = true,
+    fg = "#747992",
+  },
+}
+
+M.add = {}
+
+return M
