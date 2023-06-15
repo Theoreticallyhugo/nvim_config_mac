6f99d7df97752746321ba4362ac28f3d72f356b5
Hugo Meinhof <hugo@raballo.de>
2023-06-15 13:00:18 +0200

    added line break, fixed comment colour but not italic...

--- chadrc.lua
+++ chadrc.lua
@@ -9,7 +9,8 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = 'catppuccin',
   hl_override = highlights.override,
-  hl_add = highlights.add, nvdash = {
+  hl_add = highlights.add,
+  nvdash = {
     load_on_startup = true,
     header = {
     "┬ ┬┌─┐┬ ┬  ┌─┐┌─┐┌┐┌┌┬┐  ┌─┐┌─┐┌┬┐┌─┐┬ ┬  ┌─┐┌─┐┌─┐┌─┐┌─┐┌─┐  ",
