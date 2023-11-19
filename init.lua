85b5a45c7b8941bb2f4fa9b21e7f047cf99c87df
Theoreticallyhugo <me@theoreticallyhugo.com>
2023-11-19 16:22:13 +0100

    allow esc to be overridden by floating windws like lazygit

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 -- this means that tabs are actual \t and not spaces
 -- vim.cmd("set noexpandtab")
 
--- unset keymapping for the terminal
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]],{noremap=true})
+-- unset keymapping for the terminal, is helpful if the nvim terminal (<leader> + h) is used. however it interferes
+-- with lazygit. hence its commented out.
+-- vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]],{noremap=true})
 
